f1064461b87e1a470ba6c7c3dcce5a029ef9d133
Daniel Morao <13-10932@usb.ve>
2020-05-20 00:17:13 -0400

    DOrmir

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
+// Get all Products
+router.get("/", async (req, res) => {
+  const products = await Product.find().sort("name");
+  res.send(products);
+});
+
+// Create a product
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) res.status(400).send(error.details[0].message);
 
   // const genre = await Genre.findById(req.body.genreId);
   // if (!genre) return res.status(400).send("Invalid genre.");
-
   let product = new Product({
     name: req.body.name,
     price: req.body.price,
